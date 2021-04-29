9daa68e1ef2d5a95df6e0c6268324f9f0308271d
斑鸠0508 <ikaruga0508@gmail.com>
2021-04-30 02:20:08 +0800

    集成stacking功能，新增了频度编码和目标编码

--- _main.py
+++ _main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from ._loaders import DataLoaderBase
 from ._consts import TianchiConsts
 from ._utilities import def_log
@@ -5,7 +7,7 @@ import abc
 import os
 import random
 import numpy as np
-from pandas import DataFrame
+from pandas import DataFrame, Series
 from typing import List, Callable
 import pickle
 
@@ -133,46 +135,47 @@ class DataFrameDataMakerBase(MainBase):
         return 'test.pkl'
 
     @abc.abstractmethod
-    def _pre_process(self, df: DataFrame) -> DataFrame:
+    def _pre_process(self, df: DataFrame, train_idx, test_idx, labels, label_name) -> DataFrame:
         """预处理"""
         pass
 
     @abc.abstractmethod
-    def _drop_features(self, df: DataFrame) -> DataFrame:
+    def _drop_features(self, df: DataFrame, train_idx, test_idx, labels, label_name) -> DataFrame:
         """删除不要的特征"""
         pass
 
     @abc.abstractmethod
-    def _fillna(self, df: DataFrame) -> DataFrame:
+    def _fillna(self, df: DataFrame, train_idx, test_idx, labels, label_name) -> DataFrame:
         """填充必要的NA"""
         pass
 
     @abc.abstractmethod
-    def _fillna_others(self, df: DataFrame) -> DataFrame:
+    def _fillna_others(self, df: DataFrame, train_idx, test_idx, labels, label_name) -> DataFrame:
         """当模型不允许传入NA时，填充剩余所有NA"""
         pass
 
     @abc.abstractmethod
-    def _convert_features(self, df: DataFrame) -> DataFrame:
+    def _convert_features(self, df: DataFrame, train_idx, test_idx, labels, label_name) -> DataFrame:
         """处理或转换现有的特征"""
         pass
 
     @abc.abstractmethod
-    def _new_features(self, df: DataFrame) -> DataFrame:
+    def _new_features(self, df: DataFrame, train_idx, test_idx, labels, label_name) -> DataFrame:
         """使用现有的特征生成新的特征"""
         pass
 
     @abc.abstractmethod
-    def _drop_features_handled(self, df: DataFrame) -> DataFrame:
+    def _drop_features_handled(self, df: DataFrame, train_idx, test_idx, labels, label_name) -> DataFrame:
         """其他处理完毕后，删除不要的特征"""
         pass
 
     @abc.abstractmethod
-    def _post_process(self, df: DataFrame) -> DataFrame:
+    def _post_process(self, df: DataFrame, train_idx, test_idx, labels, label_name) -> DataFrame:
         """后处理"""
         pass
 
-    def make(self, udf: DataFrame, train_idx, test_idx, labels, label_name, folder, description, pipeline):
+    def make(self, udf: DataFrame, train_idx, test_idx, labels, label_name, folder, description, pipeline,
+             contain_labels: bool = True):
         """生成处理过后的训练集和测试集
         Args:
             udf: 合并后的数据集，使用DataFrameLoaderBase.get_union_samples()可以获得
@@ -183,11 +186,23 @@ class DataFrameDataMakerBase(MainBase):
             folder: 数据生成在哪个子目录下
             description: 表述语句，用于日志输出
             pipeline: 处理的流水线
+            contain_labels: 数据在传入流水线前是否拼接上标签(为了TargetEncoding)
         """
         self.log(description)
+        # 为了不影响后续其他的生成处理，创建拷贝
         udf_cloned = udf.copy()
+
+        # 是否拼接上标签列
+        if contain_labels:
+            udf_cloned[label_name] = labels
+
+        # 执行流水线
         for f in pipeline:
-            udf_cloned = f(udf_cloned)
+            udf_cloned = f(udf_cloned, train_idx, test_idx, labels, label_name)
+
+        # 去除标签
+        if contain_labels:
+            udf_cloned.drop(columns=[label_name], inplace=True)
 
         os.makedirs(os.path.join(self.consts.tmp_folder, folder), exist_ok=True)
 
@@ -201,3 +216,134 @@ class DataFrameDataMakerBase(MainBase):
         test_filepath = os.path.join(self.consts.tmp_folder, folder, self.get_test_data_filename())
         test_df.to_pickle(test_filepath, protocol=pickle.DEFAULT_PROTOCOL)
         self.log('共计{}行，{}列的数据保存至`{}`文件中'.format(len(test_df), len(test_df.columns), test_filepath))
+
+    def freq_encoding(self, targets: List, udf: DataFrame, train_idx, aggregate_on_all: bool = False,
+                      new_column_names: List = None) -> DataFrame:
+        """频度编码
+        Args:
+            targets: 目标列数组
+            udf: 合并后的数据集
+            train_idx: 训练集索引
+            aggregate_on_all: 是否在合并后的数据上做聚合计算(False的话只在训练集上)
+            new_column_names: 新列的名字数组
+                              如果为None的话，全部默认为`{target}_freq`
+                              如果不为None，数组长度需要和targets相同，并且其中如果存在None元素，则设置为`{target}_freq`
+        """
+        if new_column_names is None:
+            new_column_names = [None] * len(targets)
+
+        udf_cloned = udf.copy()
+
+        for target, new_column_name in zip(targets, new_column_names):
+            if aggregate_on_all:
+                aggr_data = udf[target]
+            else:
+                aggr_data = udf.loc[train_idx, target]
+
+            if new_column_name is None:
+                new_column_name = '{}_freq'.format(target)
+
+            values = aggr_data.value_counts() / len(aggr_data)
+            values.name = new_column_name
+            udf_cloned = udf_cloned.merge(values, how='left', left_on=target, right_index=True)
+
+        return udf_cloned
+
+    def target_encoding(self, targets, udf: DataFrame, train_idx, test_idx, labels, label_name,
+                        aggregate_func: Callable[[DataFrame, str, str], Series] = None,
+                        new_column_names: List = None, aggregate_name: str = 'mean',
+                        n_splits=5, stratified=True, shuffle=True, shuffle_in_indices=True, verbose=True,
+                        random_state: int = None
+                        ) -> DataFrame:
+        """目标编码
+        Args:
+            targets: 目标列数组
+            udf: 合并后的数据集
+            train_idx: 训练集索引
+            test_idx: 测试集索引
+            labels: 标签
+            label_name: 标签的列名
+            aggregate_func: 聚合函数
+            new_column_names: 新列的名字数组
+                              如果为None的话，全部默认为`{target}_{aggregate_name}`
+                              如果不为None，数组长度需要和targets相同，并且其中如果存在None元素，则设置为`{target}_{aggregate_name}`
+            aggregate_name: 聚合名称
+
+            # 以下参数用于get_train_sample_indices_for_kfold()
+            n_splits: 折数
+            stratified: 是否分层(保持标签的分布相同)
+            shuffle: 在K折处理中是否打乱顺序(该参数传递给KFold或者StratifiedKFold)
+            shuffle_in_indices: KFold或者StratifiedKFold返回的索引顺序是从小到大的，是否打乱索引顺序
+            verbose: 打印Debug信息
+            random_state: 随机状态
+        """
+        if new_column_names is None:
+            new_column_names = [None] * len(targets)
+
+        new_column_names = list(map(
+            lambda x: '{}_{}'.format(x[0], aggregate_name) if x[1] is None else x[1],
+            zip(targets, new_column_names)
+        ))
+
+        if aggregate_func is None:
+            aggregate_func = self.aggr_mean
+
+        udf_cloned = udf.copy()
+        train_df = udf.loc[train_idx].copy()
+        test_df = udf.loc[test_idx].copy()
+
+        if label_name not in train_df:
+            train_df[label_name] = labels
+
+        X_indices_list, X_val_indices_list = self.data_loader.get_train_sample_indices_for_kfold(
+            n_splits, stratified, shuffle, shuffle_in_indices, verbose, random_state)
+
+        new_columns_dict = {}
+        # 对训练集数据进行填充
+        for X_indices, X_val_indices in zip(X_indices_list, X_val_indices_list):
+            # 通过get_train_sample_indices_for_kfold()得到的索引都是基于0开始的[0, N-1]，真实索引可能乱序也不一定基于0
+            # 所以需要通过以下代码获得真正的索引
+            X_indices = train_idx[X_indices]
+            X_val_indices = train_idx[X_val_indices]
+
+            # X_val_indices上数据的编码来源于X_indices上数据的聚合运算
+            for target, new_column_name in zip(targets, new_column_names):
+                new_columns_dict.setdefault(new_column_name, [])
+
+                # 获得用于聚合运算的数据
+                raw_values = train_df.loc[X_indices, [target, label_name]]
+                agg_values = aggregate_func(raw_values, target, label_name)
+                agg_values.name = new_column_name
+                values = train_df.loc[X_val_indices].merge(
+                    agg_values, how='left', left_on=target, right_index=True)[[target, new_column_name]]
+                new_columns_dict[new_column_name].append(values)
+
+        # 对测试集数据进行填充
+        # 如果某原始列的某值在训练集中存在，则使用该值对应新列所有值的平均值
+        # 如果不存在(连接后会导致新值为NA)，则使用新列所有值的平均值来填充NA
+        for target, new_column_name in zip(targets, new_column_names):
+            values_on_train = pd.concat(new_columns_dict[new_column_name])
+            agg_on_train = values_on_train.groupby(target).mean(new_column_name)
+            values = test_df.merge(
+                agg_on_train, how='left', left_on=target, right_index=True)[[target, new_column_name]]
+            # 原始列本来就是NA的不进行填充
+            values.loc[~values[target].isna(), new_column_name] = values_on_train.mean()
+            new_columns_dict[new_column_name].append(values)
+
+        for new_column_name, values in new_columns_dict.items():
+            udf_cloned[new_column_name] = pd.concat(values)[new_column_name]
+
+        return udf_cloned
+
+    @staticmethod
+    def aggr_mean(df: DataFrame, target, label_name) -> Series:
+        """均值编码
+        Args:
+            df: 聚合运算的数据集
+            target: 目标列
+            label_name: 标签的列名
+
+        Returns:
+            均值编码后的Series对象
+        """
+        return df.groupby(target).agg({label_name: 'mean'})[label_name]
--- ml/_main.py
+++ ml/_main.py
@@ -8,6 +8,8 @@ from .._main import MainBase
 from .._loaders import DataFrameLoaderBase
 from typing import Callable, List
 from pandas import DataFrame, Series
+import numpy as np
+import glob
 
 __all__ = [
     'MachineLearningMainBase',
@@ -74,6 +76,7 @@ class MachineLearningMainBase(MainBase):
             stratified: bool = True,
             result_filepath: str = None,
             result_folder_for_ensemble: str = None,
+            random_state=None
     ) -> None:
         """执行训练与预测(交叉验证模式)
         Args:
@@ -89,14 +92,16 @@ class MachineLearningMainBase(MainBase):
                       返回值: 无
             n_splits: 折数
             stratified: 是否分层(保持标签的分布相同)
-            result_filepath: 预测结果保存的路径(中间需要有且仅有一个占位符，折数将填入其中)
+            result_filepath: 预测结果保存的路径(折数将自动拼接在文件名后，后缀名前)
             result_folder_for_ensemble: 用于模型融合的预测结果保存目录
                                         训练集的预测结果将携带索引和真实结果保存成 X_result_{第N折}.pkl
                                         验证集的预测结果将携带索引和真实结果保存成 X_val_result_{第N折}.pkl
                                         测试集的预测结果将携带id保存成 X_test_result_{第N折}.pkl
+            random_state: 随机状态，该值将被传入get_train_sample_indices_for_kfold()以保证K折结果确定化
         """
         self.log('开始训练({}折交叉验证)'.format(n_splits))
-        X_indices_list, X_val_indices_list = dl.get_train_sample_indices_for_kfold(n_splits, stratified)
+        X_indices_list, X_val_indices_list = dl.get_train_sample_indices_for_kfold(
+            n_splits, stratified, random_state=random_state)
         labels = dl.get_train_labels()
         ids = dl.get_test_ids()
 
@@ -105,6 +110,11 @@ class MachineLearningMainBase(MainBase):
 
         for i, (X_indices, X_val_indices) in enumerate(zip(X_indices_list, X_val_indices_list)):
             self.log('执行第{}折'.format(i + 1))
+            # 通过get_train_sample_indices_for_kfold()得到的索引都是基于0开始的[0, N-1]，真实索引可能乱序也不一定基于0
+            # 所以需要通过以下代码获得真正的索引
+            X_indices = dl.train_samples.index[X_indices]
+            X_val_indices = dl.train_samples.index[X_val_indices]
+
             X = dl.train_samples.loc[X_indices].drop(columns=[dl.get_label_column()])
             y = labels.loc[X_indices]
             X_val = dl.train_samples.loc[X_val_indices].drop(columns=[dl.get_label_column()])
@@ -115,7 +125,9 @@ class MachineLearningMainBase(MainBase):
             fit_func(X, y, X_val, y_val)
 
             if result_filepath is not None:
-                filepath = result_filepath.format(i + 1)
+                # 将第几折信息拼接在文件名最后，后缀名前
+                path_root, ext = os.path.splitext(result_filepath)
+                filepath = '{}_{}{}'.format(path_root, i + 1, ext)
                 folder = os.path.dirname(filepath)
                 if os.path.exists(folder):
                     y_pred_on_X_test = predict_func(X_test)
@@ -147,6 +159,77 @@ class MachineLearningMainBase(MainBase):
 
         if complete_func is not None:
             complete_func(pd.concat(y_preds_on_X_val))
+
+    def get_default_output_folder_for_ensemble(self, name):
+        """获得默认的集成学习中间文件的输出目录
+        Args:
+            name: 最后一层目录名。方便区别通常指定为模型的名字: xgb, lgb等
+        """
+        return os.path.join(self.get_default_ensemble_root(), name)
+
+    def get_default_ensemble_root(self):
+        """获得默认的集成学习根目录"""
+        return os.path.join(self.consts.output_folder, 'ensemble')
+
+    def stacking(self,
+                 targets: List,
+                 fit_func: Callable[[DataFrame, Series], None],
+                 predict_func: Callable[[DataFrame], List],
+                 result_filepath: str,
+                 ensemble_root_folder=None,
+                 ) -> None:
+        """Stacking融合
+        Args:
+            targets: 参与融合的第一层模型列表
+            fit_func: 训练回调函数，需在里面完成fit()
+                      参数: 训练集数据, 训练集标签
+                      返回值: 无
+            predict_func: 预测回调函数，需在里面完成predict()或者predict_proba()
+                      参数: 测试集数据
+                      返回值: 预测结果
+            result_filepath: 预测结果保存的路径
+            ensemble_root_folder: 用于模型融合的中间数据的根目录(不设置则将自动设置为get_default_ensemble_root()返回的目录)
+        """
+        if ensemble_root_folder is None:
+            ensemble_root_folder = self.get_default_ensemble_root()
+
+        # 融合对象的存在性检查
+        dirs_not_exists = list(filter(lambda x: not os.path.isdir(os.path.join(ensemble_root_folder, x)), targets))
+        if len(dirs_not_exists) > 0:
+            self.log('以下对象集成学习的目录不存在，请先使用K折交叉验证方式创建')
+            self.log('\t{}'.format(', '.join(dirs_not_exists)))
+            return
+
+        # 第二层训练集
+        train_ds = []
+        # 第二层测试集
+        test_ds = []
+        # 标签
+        labels = None
+        for target in targets:
+            folder = os.path.join(ensemble_root_folder, target)
+            self.log('读取`{}`中的数据'.format(folder))
+
+            # 使用对验证集的预测结果拼接第二层的训练集
+            df = pd.concat([pd.read_pickle(path) for path in glob.glob(os.path.join(folder, 'X_val_result_*.pkl'))])
+            train_ds.append(pd.Series(df['y_pred'].values, index=df.index, name=target))
+            if labels is None:
+                labels = df['y_true']
+
+            # 使用对测试集的预测结果就平均作为第二层的测试集
+            results = [pd.read_pickle(path) for path in glob.glob(os.path.join(folder, 'X_test_result_*.pkl'))]
+            ids = results[0].index
+            test_ds.append(pd.Series(np.squeeze(np.mean(np.array(results), axis=0), axis=-1), index=ids, name=target))
+
+        train_ds.append(labels)
+        # 不同模型数据连接时，将根据索引自动对齐
+        train_ds = pd.concat(train_ds, axis=1)
+        test_ds = pd.concat(test_ds, axis=1)
+
+        # 第二层训练
+        fit_func(train_ds.drop(columns='y_true'), train_ds['y_true'])
+        y_pred = predict_func(test_ds)
+        self.save_results(y_pred, test_ds.index, result_filepath)
 
     @abc.abstractmethod
     def save_results(self, y_pred, ids: List, filepath: str) -> None:
